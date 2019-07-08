a5fd3ae257d29466512d0346a8407a0fb4633572
VulpineReynard <milare8@gmail.com>
2019-07-08 11:48:22 -0400

    MVP Complete - scores updated correctly

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ function App() {
   const [homeScore = 0, setHomeScore] = useState();
   const [awayScore = 0, setAwayScore] = useState();
 
-
-
   return (
     <div className="container">
       <section className="scoreboard">
@@ -32,12 +30,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick={() => setHomeScore(homeScore + 7)} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={() => setHomeScore(homeScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={() => setAwayScore(awayScore + 7)} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={() => setAwayScore(awayScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
